1682362d03f1878626bab356e3696dd03bac5d4c
mohamed.ajel <ajelmohamed03@gmail.com>
2020-02-14 11:34:17 +0100

    firstcommit

---
+++ Eureka-Client/src/test/java/com/Client/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Client.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
